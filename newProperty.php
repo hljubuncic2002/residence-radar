70125690faefee86f3f88f137100f9848de44ee8
Haris Ljubuncic <99652252+hljubuncic2002@users.noreply.github.com>
2024-04-29 21:39:24 -0400

    Update newProperty.php

--- newProperty.php
+++ newProperty.php
@@ -2,7 +2,7 @@
 session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
-$conn = new mysqli('localhost', 'hljubuncic1', 'hljubuncic1', 'hljubuncic1');
+$conn = new mysqli('localhost', '', '', 'residence radar');
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
